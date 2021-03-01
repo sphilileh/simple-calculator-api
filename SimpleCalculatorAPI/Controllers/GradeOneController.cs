a92e5ef77aa8803a0cd5576ccbec08cac14f7f13
Sphilile Dladla <dladlasphilile88@gamil.com>
2021-03-01 15:11:13 +0200

    initial commit

---
+++ SimpleCalculatorAPI/Controllers/GradeOneController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleCalculatorAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SimpleCalculatorAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class GradeOneController : ControllerBase
+	{
+		// GET: api/<GradeOneController>
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+
+		// POST api/<GradeOneController>
+		[HttpPost("add")]
+		public int PostAdd([FromBody] TwoNumbersModel value)
+		{
+			return value.Number1 + value.Number2;
+		}
+
+		// POST api/<GradeOneController>
+		[HttpPost("subtract")]
+		public int PostSub([FromBody] TwoNumbersModel value)
+		{
+			return value.Number1 - value.Number2;
+		}
+
+
+	}
+}
---
+++ SimpleCalculatorAPI/Models/TwoNumbersModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCalculatorAPI.Models
+{
+	public class TwoNumbersModel
+	{
+		public int Number1 { get; set; }
+		public int Number2 { get; set; }
+	}
+}
